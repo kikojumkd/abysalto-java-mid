a32f2bc66ecaffcd712f9d66857761c13c00918c
kristijan.junuzovski <kristijanjunuzovski@gmail.com>
2026-02-07 16:07:18 +0100

    change min password length from 6 to 8

--- src/main/java/hr/abysalto/hiring/mid/dto/request/RegisterRequest.java
+++ src/main/java/hr/abysalto/hiring/mid/dto/request/RegisterRequest.java
@@ -21,7 +21,7 @@ public class RegisterRequest {
     private String email;
 
     @NotBlank(message = "Password is required")
-    @Size(min = 6, max = 100, message = "Password must be between 6 and 100 characters")
+    @Size(min = 8, max = 100, message = "Password must be between 8 and 100 characters")
     private String password;
 
     @NotBlank(message = "First name is required")
